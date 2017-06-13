f598af5adbcf6e0de2da4d5cd9b7a7d92c0cd05e
aidys98 <super.suvan@mail.ru>
2017-06-13 20:55:31 +0700

    Пофиксил программу

--- kwadrat_uravnenie.cpp
+++ kwadrat_uravnenie.cpp
@@ -11,21 +11,19 @@ cout << "Vvedite koeficienty uravneniya " << endl;
 cin >> a; 
 cin >> b; 
 cin >> c; 
-if (a!=0 && b!=0 && c!=0) { 
-D=b*b-4*a*c; 
-if (D>0) { 
-x1=-(b+sqrt(D))/(2*a); 
-x2=-(b-sqrt(D))/(2*a); 
-cout << "D="<< D << endl; 
+if (a!=0 && b!=0 && c==0) { 
+x1=0; 
+x2=-(b/a); 
 cout << "x1=" << x1 << endl; 
 cout << "x2=" << x2 << endl; 
 } 
-if (D == 0) { 
-x1=-b/(2*a); 
-cout << "D=0" << endl; 
-cout << "x=" << x1 << endl; 
-} 
-if (D<0) cout << "Net resheniy" << endl; 
+if (a!=0 && b==0 && c!=0) { 
+x1=sqrt(c/a); 
+x2=-sqrt(c/a); 
+cout << "x1=" << x1 << endl; 
+cout << "x2=" << x2 << endl; 
+}
+if (D<0) cout << "Net resheniy" << endl; {
 }
 system("pause"); 
 return 0; }
