e9abae15f47e8bb316405929b53ca8f55d17a04f
aidys98 <super.suvan@mail.ru>
2017-06-13 20:49:30 +0700

    Добавил нахождение дискриминанта

--- kwadrat_uravnenie.cpp
+++ kwadrat_uravnenie.cpp
@@ -10,7 +10,22 @@ float D,x1,x2;
 cout << "Vvedite koeficienty uravneniya " << endl; 
 cin >> a; 
 cin >> b; 
-cin >> c;
-system("pause"); 
-return 0; 
+cin >> c; 
+if (a!=0 && b!=0 && c!=0) { 
+D=b*b-4*a*c; 
+if (D>0) { 
+x1=-(b+sqrt(D))/(2*a); 
+x2=-(b-sqrt(D))/(2*a); 
+cout << "D="<< D << endl; 
+cout << "x1=" << x1 << endl; 
+cout << "x2=" << x2 << endl; 
+} 
+if (D==0) { 
+x1=-b/(2*a); 
+cout << "D=0" << endl; 
+cout << "x=" << x1 << endl; 
+} 
+if (D<0) cout << "Net resheniy" << endl; 
 }
+system("pause"); 
+return 0; }
