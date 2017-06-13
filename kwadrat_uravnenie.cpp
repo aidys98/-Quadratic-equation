e8e23964ef7909f858a217026fc63934e769b23e
aidys98 <super.suvan@mail.ru>
2017-06-13 20:46:00 +0700

    Добавили ввод данных в программу

--- kwadrat_uravnenie.cpp
+++ kwadrat_uravnenie.cpp
@@ -5,7 +5,12 @@
 using namespace std; 
 
 int main() 
-{
+{ int a,b,c; 
+float D,x1,x2; 
+cout << "Vvedite koeficienty uravneniya " << endl; 
+cin >> a; 
+cin >> b; 
+cin >> c;
 system("pause"); 
 return 0; 
 }
